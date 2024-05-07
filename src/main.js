040725d6d2d6bc071f66024e6a8ed494064ad8dc
Karen Dantas <karennobre74@gmail.com>
2024-05-07 17:32:49 -0300

    feat: add forms validation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ const form =document.getElementById('form');
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
+
+    const isNameValid = validateName(nameInput);
+    const isLoginValid = validateLogin(loginInput);
+    const isEmailValid = validateEmail(emailInput);
+    const isPasswordValid = validatePassword(passwordInput);
+
+    console.log(isEmailValid);
+   if( isNameValid && isLoginValid && isEmailValid && isPasswordValid){
+        forms.submit()
+        alert('Formulário enviado!')
+   }
 })
 
 
@@ -11,3 +22,37 @@ button.addEventListener('click',
         button.style= 'transform:translateY(5px)';
     }
 )
+const spans = document.getElementsByClassName("error-message");
+
+const nameInput = document.getElementById('name');
+const loginInput = document.getElementById('login');
+const emailInput = document.getElementById('email')
+const passwordInput = document.getElementById('password');
+const gamesInput = document.getElementById('games');
+
+function validateName(name){
+    return (name.value.length< 3) ? setErrorSpan(0) : removeErrorSpan(0);
+}
+function validateLogin(login){
+    return (login.value.length< 3) ? setErrorSpan(1) : removeErrorSpan(1);
+}
+
+function validateEmail(email){
+    const regex = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/
+    const isTrue = regex.test(email.value)
+    return isTrue ? removeErrorSpan(2) : setErrorSpan(2);
+    
+}
+function validatePassword(password){
+   return (password.value.length < 6) ? setErrorSpan(3) : removeErrorSpan(3);
+}
+
+//Span styles
+function setErrorSpan(index){
+    spans[index].style.display = "block";
+    return false;
+}
+function removeErrorSpan(index){
+    spans[index].style.display = "none";
+    return true;
+}
